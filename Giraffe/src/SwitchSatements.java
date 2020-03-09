6ceb89c019d6dd400dca56cf7d1870195beb05c7
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2020-03-09 10:52:43 -0400

    switch statements

---
+++ Giraffe/src/SwitchSatements.java
@@ -0,0 +1,52 @@
+
+public class SwitchSatements {
+	public static void main (String [] args)
+	{
+		System.out.println(getDayName(4));
+	}
+		public static String getDayName(int dayNum)
+		{
+			String dayName= "";
+			
+			switch(dayNum)
+			{
+			case 0: 
+				dayName = "Sunday";
+					break;
+					
+			case 1: 
+				dayName = "Monday";
+					break;
+					
+			case 2: 
+				dayName = "Tuesday";
+					break;
+					
+			case 3: 
+				dayName = "Wednesday";
+					break;
+					
+			case 4: 
+				dayName = "Thursday";
+					break;
+					
+			case 5: 
+				dayName = "Friday";
+					break;
+					
+			case 6: 
+				dayName = "Saturday";
+					break;
+						
+			default: dayName = "invalid day number";
+					break;
+			
+			}
+			
+			
+			return dayName;
+		
+		
+	}
+
+}
