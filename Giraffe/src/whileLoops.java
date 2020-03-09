842fdf71f9bfe22c5f3a3e8cef96d31dcd5296a1
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2020-03-09 12:02:40 -0400

    while loop practice

---
+++ Giraffe/src/whileLoops.java
@@ -0,0 +1,20 @@
+
+public class whileLoops {
+	public static void main(String [] args)
+	{
+		//indexing variable (counter)
+		int i = 1;
+		
+		while(i <= 10)
+		{
+			System.out.println(i);
+//			i = i + 1;
+			i++;
+		}
+		
+		
+		
+		
+	}
+
+}
