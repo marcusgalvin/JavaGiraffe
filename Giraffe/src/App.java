8be5703dca9cd9927e42710cb2a0b8c28b37405d
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2020-03-09 17:08:14 -0400

    building a student class and object

--- Giraffe/src/App.java
+++ Giraffe/src/App.java
@@ -4,13 +4,16 @@ public class App {
 
 	public static void main(String[] args)
 	{
-		Scanner keyboardInput = new Scanner(System.in);
-		System.out.print("The first number is : ");
-		double num1 = keyboardInput.nextDouble();
-		System.out.print("The second number is : ");
-		double num2 = keyboardInput.nextDouble();
+		Student myStudent = new Student();
+		myStudent.firstName = "Jim";
+		myStudent.lastName = "Halpert";
 		
-		System.out.println(num1 + num2);
+		myStudent.major = "Buisness";
+		myStudent.gpa = 2.3;
+		myStudent.age = 24;
+		myStudent.onProbation = false;
+		
+		System.out.println(myStudent.firstName);
 		
 	}
 	
---
+++ Giraffe/src/Student.java
@@ -0,0 +1,12 @@
+
+public class Student {
+
+	String firstName;
+	String lastName;
+	double gpa;
+	String major;
+	int age;
+	boolean onProbation;
+	
+	
+}
