aca7f6ad80feed2ee7d58364149e0b739106199d
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2020-03-10 10:55:21 -0400

    up to inheritance lecture

---
+++ Giraffe/src/classAttributes/App.java
@@ -0,0 +1,9 @@
+package classAttributes;
+
+public class App 
+{
+	public static void main(String [] args)
+	{
+		
+	}
+}
---
+++ Giraffe/src/classAttributes/Song.java
@@ -0,0 +1,18 @@
+package classAttributes;
+
+public class Song {
+	private String title;
+	private String artist;
+	private int length;
+	
+	public Song(String title, String artist, int length)
+	{
+		this.title = title;
+		this.artist = artist;
+		this.length = length;
+	}
+
+	
+	
+	
+}
